38351e328eaff985110db85047d5d6fa25acaa9a
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2025-09-10 12:13:43 +0530

    Update storage_integration.sql

--- storage_integration.sql
+++ storage_integration.sql
@@ -4,3 +4,6 @@ STORAGE_PROVIDER = S3
 ENABLED = TRUE
 STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::074116122235:role/aaryan-s3-snowpipe-project-role'
 STORAGE_ALLOWED_LOCATIONS = ('s3://aaryan-snowflake-project-bucket/');
+
+
+DESC STORAGE INTEGRATION s3_snowpipe_integration;
